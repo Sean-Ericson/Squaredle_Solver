7a45d3dc80b80dac8b184c0523d5f681bd92bca4
Sean-Ericson <sericson314@gmail.com>
2025-12-10 08:42:12 -0800

    data

--- solver.py
+++ solver.py
@@ -40,44 +40,54 @@ def create_graph(grid):
     graph = create_grid_with_diagonals(*grid.shape)
     for node in list(graph.nodes):
         x,y = node
-        graph.nodes[node]['letter'] = grid[y, x]
-        if grid[y,x] == '': 
+        graph.nodes[node]['letter'] = grid[x,y]
+        if grid[x,y] == '': 
             graph.remove_node(node)
     return graph
 
-def find_words(graph, words_by_length):
-    found_words = set()
+def find_word_paths(graph, words_by_length):
+    word_paths = {}
 
     def determine_consistent_words(current_words_by_len, current_string):
         return {i: [w for w in current_words_by_len[i] if w.startswith(current_string)] for i in range(max(len(current_string), 4), max(words_by_length.keys()) + 1)}
 
-    def recursive_search(current_string, current_node, current_graph, consistent_words):
+    def recursive_search(current_path, current_graph, consistent_words):
         #print(current_string, current_node, consistent_words.keys())
         if all(len(consistent_words[i]) == 0 for i in consistent_words.keys()):
             return
+        current_node = current_path[-1]
+        current_string = "".join(graph.nodes[n]['letter'] for n in current_path)
         for neighbor in current_graph.neighbors(current_node):
             next_string = current_string + graph.nodes[neighbor]['letter']
+            next_path = current_path + [neighbor]
+            if next_string == '':
+                print(current_path, next_path)
+                raise ValueError("ahh")
             if len(next_string) >= MIN_WORD_LENGTH and next_string in consistent_words[len(next_string)]:
-                found_words.add(next_string)
+                if next_string not in word_paths:
+                    word_paths[next_string] = [next_path]
+                else:
+                    word_paths[next_string].append(next_path)
             next_consistent_words = determine_consistent_words(consistent_words, next_string)
             new_graph = current_graph.copy()
             new_graph.remove_node(current_node)
-            recursive_search(next_string, neighbor, new_graph, next_consistent_words)
+            recursive_search(next_path, new_graph, next_consistent_words)
 
     for node in graph.nodes:
-        starting_letter = graph.nodes[node]['letter']
         new_graph = graph.copy()
         new_graph.remove_node(node)
-        recursive_search(starting_letter, node, graph, words_by_length)
+        recursive_search([node], graph, words_by_length)
 
-    return list(found_words)
+    return word_paths
 
-def solve(grid_str):
+def solve(grid_str, return_paths=False) -> dict[str, list[list[tuple[int, int]]]] | list[str]:
     words_by_length = get_words_by_length()
     grid = string_to_grid(grid_str)
     graph = create_graph(grid)
-    found_words = find_words(graph, words_by_length)
-    return found_words
+    word_paths = find_word_paths(graph, words_by_length)
+    if return_paths:
+        return word_paths
+    return list(word_paths.keys())
 
 def solve_and_autoclick(grid_str, delay=2):
     found_words = solve(grid_str)
